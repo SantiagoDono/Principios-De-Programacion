27453ee86af0fe45c8a1c2945f3bbf4507261837
santiago dono <santiago858@hotmail.com>
2022-12-07 23:27:50 -0300

    ejercicio 5 de los primos

--- Clase 2/Clase 2.cpp
+++ Clase 2/Clase 2.cpp
@@ -2,18 +2,33 @@
 //
 
 #include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+int main(){
+    // ejercicio 5 hayar un numero primo
+    int numero, div, resto,cont;
+    cout << "Ingrese un numero: " << endl;
+    cin >> numero;
+     div = 1;
+     cont = 0;
+    while (div <= numero) {
+        
+        resto = numero % div;
+        if (resto == 0) {
+-            cont++;
+        }
+            div++;
 
-int main()
-{
+    }
+    if (cont == 2) {
+        cout << numero << " Es primo." << endl;
+    }
+    else {
+        cout << numero << " No es primo." << endl;
+    }
+   
+    return 0;
     
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
