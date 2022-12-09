cb0ac4d8fd3b35b25a3a776bdf40c830f70d16ce
santiago dono <santiago858@hotmail.com>
2022-12-09 19:57:10 -0300

    ejercicio 8 listo

--- Clase 2/Clase 2.cpp
+++ Clase 2/Clase 2.cpp
@@ -5,60 +5,32 @@
 #include<stdio.h>
 #include<stdlib.h>
 using namespace std;
-//int main(){
-//    // ejercicio 5 hayar un numero primo
-//    int numero, div, resto,cont;
-//    cout << "Ingrese un numero: " << endl;
-//    cin >> numero;
-//     div = 1;
-//     cont = 0;
-//    while (div <= numero) {
-//        
-//        resto = numero % div;
-//        if (resto == 0) {
-//-            cont++;
-//        }
-//            div++;
-//
-//    }
-//    if (cont == 2) {
-//        cout << numero << " Es primo." << endl;
-//    }
-//    else {
-//        cout << numero << " No es primo." << endl;
-//    }
-//
-//    //ejercicio 6
-//
-//    return 0;
-//    
-//}
+int main(){
+    // ejercicio 5 hayar un numero primo
+    int numero, div, resto,cont;
+    cout << "Ingrese un numero: " << endl;
+    cin >> numero;
+     div = 1;
+     cont = 0;
+    while (div <= numero) {
+        
+        resto = numero % div;
+        if (resto == 0) {
+-            cont++;
+        }
+            div++;
 
-int main() {
-	int numero, div, cont, resto;
+    }
+    if (cont == 2) {
+        cout << numero << " Es primo." << endl;
+    }
+    else {
+        cout << numero << " No es primo." << endl;
+    }
 
-	cout << "Ingrese un numero:" << endl;
-	cin >> numero;
-	cont = 1;
-	div = 1;
-	while (div <= numero)
-	{
-		resto = numero % div;
-		if (resto == 0)
-		{
-			cont++;
+    //ejercicio 6
 
-		}
-			div++;
-		if (cont == 2) {
-			cout << numero << endl;
-			numero--;
-			cont = 1;
-			div = 1;
-
-		}
-			numero--;
-		
-	}
-	return 0;
+    return 0;
+    
 }
+
--- Ejercicio 8/Ejercicio 8.cpp
+++ Ejercicio 8/Ejercicio 8.cpp
@@ -2,18 +2,35 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include < stdlib.h>
 
+using namespace std;
+// el usuario ingresa 5 numeros y se debe indicar cual de estos es el mayor y cual es el menor
 int main()
 {
-    std::cout << "Hello World!\n";
+    int numero,mayor,menor,contador;
+    contador = 4;
+        cout << " ingrese un numero " << endl;
+        cin >> numero;
+        mayor = numero;
+        menor = numero;
+    while (contador > 0)
+    {
+        cout << " ingrese un numero " << endl;
+        cin >> numero;
+        if (numero > mayor) {
+            mayor = numero;
+        }else
+            if (numero < menor) {
+                menor = numero;
+            }
+        contador --;
+
+    }
+    cout << " el numero mas grande ingresado es: " << mayor << endl;
+    cout << " el numero mas chico ingresado es: " << menor << endl;
+    return 0;
+    
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
