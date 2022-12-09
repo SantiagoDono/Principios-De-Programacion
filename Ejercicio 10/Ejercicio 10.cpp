e36c5d4739c6e3ec366ddb6f6ede55675d73267d
santiago dono <santiago858@hotmail.com>
2022-12-09 20:34:25 -0300

    letra del ejercicio 10

--- Ejercicio 10/Ejercicio 10.cpp
+++ Ejercicio 10/Ejercicio 10.cpp
@@ -1,10 +1,11 @@
-// Ejercicio 10.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+
 
 #include <iostream>
 #include<stdio.h>
 #include<stdlib.h>
+
 using namespace std;
+// ingresar un numero y devolverlo digito por digito (puede devolverse al reves)
 int main()
 {
 	int numero, digito;
@@ -20,12 +21,4 @@ int main()
 	return 0;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
