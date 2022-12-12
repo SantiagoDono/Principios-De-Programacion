d86ff519a7e77bbc8bb8c61f16ad43b726052ba8
santiago dono <santiago858@hotmail.com>
2022-12-11 21:09:59 -0300

    Ejercicio 12 13 y 14

--- Ejercicio 12/Ejercicio 12.cpp
+++ Ejercicio 12/Ejercicio 12.cpp
@@ -6,45 +6,164 @@
 #include<stdio.h>
 
 using namespace std;
-// calculadora que pueda hacer suma, resta, multiplicacion y division (entre dos numeros nada mas) 
+// calculadora que pueda hacer suma, resta, multiplicacion, division, elevado(no puedo usar switch todavia;
 int main()
 {
-    int num1, num2, oper, resultado;
-    cout << "ingrese la operacion que desea realizar: \n 1 para suma. \n 2 para resta. \n 3 multiplicacion. \n 4 division.\n ";
+    int num1, num2, oper, resultado,cont,ant;
+    cout << "ingrese la operacion que desea realizar: \n 1 para suma. \n 2 para resta. \n 3 multiplicacion. \n 4 division.\n 5 para elevar un numero al cuadrado\n 6 para arrancar nuevamente\n7 para salir ";
     cin >> oper;
-    cout << "ingrese el primer numero:  ";
-    cin >> num1;
-    cout << "ingrese el segundp numero: ";
-    cin >> num2;
+    cont = 0;
+    while (oper != 7) {
+       
+        if (oper != 5 and oper != 6 and cont < 1) {
+            cout << "ingrese el primer numero:  ";
+            cin >> num1;
+            cout << "ingrese el segundp numero: ";
+            cin >> num2;
+        }
+        else
+            if (oper == 5 and cont < 1) {
+                cout << " ingrese el numero a elevar: ";
+                cin >> num1;
 
-    if (oper == 1) {
-        resultado = num1 + num2;
-        cout << "el resultado de la suma es: " << resultado << endl;
-    }
-    else
-        if (oper == 2) {
-            resultado = num1 - num2;
-            cout << "el resultado de la resta es: " << resultado << endl;
-        }else
-            if (oper == 3) {
-                if (num1 != 0 and num2 != 0) {
-                    resultado = num1 * num2;
-                    cout << "el resultado de la multiplicacion es: " << resultado << endl;
-                }
-                else {
-                    cout << "el resultado es 0 debido a auqe alguno de los dos numeros es 0." << endl; 
-                }
+            }
+
+        if (oper == 1) {
+            if (cont > 0) {
+                cout << " ingrese el numero a sumar al resultado anterior: ";
+                cin >> num1;
+                resultado = ant + num1;
+                ant = resultado;
+                cout << "\n el resultado es: " << resultado; 
+                cout << "\ningrese que operacion quiere realizar: ";
+                cin >> oper;
+
             }
             else {
-                if (num1 > num2) {
-                    resultado = num1 / num2;
-                    cout << "el resultado de la division es: " << resultado;
+                resultado = num1 + num2;
+                cout << "el resultado de la suma es: " << resultado << endl;
+                cont++;
+                ant = resultado;
+                cout << "ingese que operacion quiere realizar: ";
+                cin >> oper;
+
+            }
+        }
+        else
+            if (oper == 2) {
+                if (cont > 0) {
+                    cout << "ingrese el numero a restar del resultado anterior: ";
+                    cin >> num1;
+                    resultado = ant - num1;
+                    cout << "el resultado es: " << resultado;
+                    ant = resultado;
+                    cout << "\ningrese la operacion que quiere realizar: ";
+                    cin >> oper;
                 }
                 else {
-                    resultado = num2 / num1;
-                    cout << "el resultado de la division es: " << resultado;
+                    resultado = num1 - num2;
+                    cout << "el resultado de la resta es: " << resultado << endl;
+                    cont++;
+                    ant = resultado;
+                    cout << "\n ingese que operacion quiere realizar: ";
+                    cin >> oper;
+
                 }
+                
             }
+            else
+                if (oper == 3) {
+                    if (cont > 0) {
+                        cout << "ingrese el numero que multipicara el resultado anterior: ";
+                        cin >> num1;
+                        resultado = ant * num1;
+                        cout << "El resultado es : " << resultado;
+                        ant = resultado;
+                        cout << "\nIngrese que operacion quiere realizar: ";
+                        cin >> oper;
+                    }
+                    else {
+
+                        if (num1 != 0 and num2 != 0) {
+                            resultado = num1 * num2;
+                            cout << "el resultado de la multiplicacion es: " << resultado << endl;
+                            cont++;
+                            ant = resultado;
+                            cout << "ingese que operacion quiere realizar: ";
+                            cin >> oper;
+                        }
+                        else {
+                            cout << "el resultado es 0 debido a auqe alguno de los dos numeros es 0." << endl;
+                            cout << "ingese que operacion quiere realizar: ";
+                            cin >> oper;
+                        }
+                    }
+                }
+                else
+                    if (oper == 4) {//division
+                        if (cont > 0) {
+                            cout << "ingrese el numero que divide al resultado anterior: " << endl;;
+                            cin >> num1;
+                            if (ant >= num1) {
+                                resultado = ant / num1;
+                                ant = resultado;
+                                cout << "el resultado es: " << resultado << endl;
+                                cout << "ingrese la operacion que quiere realizar";
+                                cin >> oper;
+                            }
+                            else {
+                                if (ant < num1) {
+                                    resultado = num1 / ant;
+                                    ant = resultado;
+                                    cout << "el resultado es: " << resultado << endl;
+                                    cout << "ingrese la operacion que quiere realizar";
+                                    cin >> oper;
+                                }
+                            }
+                        }else {
+                            if (num1 > num2) {
+                                resultado = num1 / num2;
+                                cout << "el resultado de la division es: " << resultado<< endl;
+                                cont++;
+                                ant = resultado;
+                                cout << "ingese que operacion quiere realizar: ";
+                                cin >> oper;
+                            }else {
+                                resultado = num2 / num1;
+                                cout << "el resultado de la division es: " << resultado << endl;
+                                cont++;
+                                ant = resultado;
+                                cout << "ingese que operacion quiere realizar: ";
+                                cin >> oper;
+                            }
+                        }
+                    }
+                    else
+                        if (oper == 5) {
+                            if (cont > 0) {
+                                resultado = ant * ant;
+                                cout << "el resultado es: " << resultado << endl;
+                                cout << "ingrese la operacion a realizar: ";
+                                cin >> oper;
+                            }
+                            else {
+                                resultado = num1 * num1;
+                                cout << "El resultado es: " << resultado;
+                                cont++;
+                                ant = resultado;
+                                cout << "ingese que operacion quiere realizar: ";
+                                cin >> oper;
+
+                            }
+                        }else 
+                            if(oper == 6){
+                                cont = 0;
+                                num1 = 0;
+                                num2 = 0;
+                                cout << "ingese que operacion quiere realizar: ";
+                                cin >> oper;
+                            }
+    }
 
 
 
