66a0d48035bb141bed89242914d6adc40f29c105
santiago dono <santiago858@hotmail.com>
2022-12-17 18:53:55 -0300

    Inicio ejercicio 21

---
+++ Ejercicio 21/Ejercicio 21.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+    char letra;
+    int A, E, I, O, U;
+    cout << "ingrese una linea con minusculas, puntos y espacios, que tenga dos puntos seguidos:" << endl;
+    cin >> letra;
+    do{
+        if(letra == '.')
+            if (letra == ',') {
+                cout << "bien";
+            }
+
+    } while (letra != '\n');
+}
+
